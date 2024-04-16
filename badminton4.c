022d78cb3e9984f14df396ad6e88137c968726b2
lyssssssss <118453995+lyssssssss@users.noreply.github.com>
2024-04-16 22:20:08 +0800

    Update badminton4.c

--- badminton4.c
+++ badminton4.c
@@ -3,38 +3,45 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv) {
-    const int PING_PONG_LIMIT = 16;
+  const int PING_PONG_LIMIT = 10;
 
-    // Initialize the MPI environment
-    MPI_Init(&argc, &argv);
+  // Initialize MPI environment
+  MPI_Init(NULL, NULL);
+  // Get the current process rank and the total number of processes
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // Find out rank, size
-    int world_rank, world_size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  // Ensure the total number of processes is 4
+  if (world_size != 4) {
+    fprintf(stderr, "World size must be four for %s\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
 
-    // Check if the world size is 4
-    if (world_size != 4) {
-        fprintf(stderr, "World size must be four for %s\n", argv[0]);
-        MPI_Abort(MPI_COMM_WORLD, 1);
-        exit(EXIT_FAILURE);
+  int ping_pong_count = 0;
+  int partner_rank = (world_rank + 1) % 4;
+  int team_mate = (world_rank + 2) % 4;  // Find the teammate in the same team
+
+  while (ping_pong_count < PING_PONG_LIMIT) {
+    if (world_rank == ping_pong_count % 4) {
+      // Increment ping_pong_count before sending it
+      ping_pong_count++;
+      // Send to the next process
+      MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
+      printf("P%d sent and incremented ping_pong_count %d to P%d\n",
+             world_rank, ping_pong_count, partner_rank);
+      // Also inform the teammate
+      MPI_Send(&ping_pong_count, 1, MPI_INT, team_mate, 0, MPI_COMM_WORLD);
+    } else {
+      // Receive the number
+      MPI_Recv(&ping_pong_count, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD,
+               MPI_STATUS_IGNORE);
+      printf("P%d received ping_pong_count %d from P%d\n",
+             world_rank, ping_pong_count, MPI_ANY_SOURCE);
     }
+  }
 
-    int ping_pong_count = 0;
-    int partner_rank = (world_rank + 1) % 4;
-    while (ping_pong_count < PING_PONG_LIMIT) {
-        if (world_rank == ping_pong_count % 4) {
-            MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
-            printf("P%d sent ping_pong_count %d to P%d\n",
-                   world_rank, ping_pong_count, partner_rank);
-            ping_pong_count++;
-        } else {
-            MPI_Recv(&ping_pong_count, 1, MPI_INT, (world_rank == 0 ? 3 : world_rank - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("P%d received ping_pong_count %d from P%d\n",
-                   world_rank, ping_pong_count, (world_rank == 0 ? 3 : world_rank - 1));
-        }
-    }
-
-    MPI_Finalize();
-    return 0;
+  MPI_Finalize();
+  return 0;
 }
