62ad06cd3031909a7be39cf9ab685df430455e79
lyssssssss <118453995+lyssssssss@users.noreply.github.com>
2024-04-16 21:41:38 +0800

    Update badminton4.c

--- badminton4.c
+++ badminton4.c
@@ -23,11 +23,11 @@ int main(int argc, char** argv) {
     int ping_pong_count = 0;
     int partner_rank = (world_rank + 1) % 4;
     while (ping_pong_count < PING_PONG_LIMIT) {
-        if (world_rank != ping_pong_count % 4) {
-            MPI_Status status;
-            MPI_Recv(&ping_pong_count, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-            printf("P%d received ping_pong_count %d from P%d\n",
-                   world_rank, ping_pong_count, status.MPI_SOURCE);
+        if (world_rank == ping_pong_count % 4) {
+            ping_pong_count++;
+            MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
+            printf("P%d sent and incremented ping_pong_count %d to P%d\n",
+                   world_rank, ping_pong_count, partner_rank);
         } else {
             MPI_Recv(&ping_pong_count, 1, MPI_INT, (world_rank == 0 ? 3 : world_rank - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             printf("P%d received ping_pong_count %d from P%d\n",
