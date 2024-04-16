735b7513b1a48b914823716365cf49d80da79e16
lyssssssss <118453995+lyssssssss@users.noreply.github.com>
2024-04-16 21:20:25 +0800

    Update badminton4.c

--- badminton4.c
+++ badminton4.c
@@ -21,21 +21,38 @@ int main(int argc, char** argv) {
     }
 
     int ping_pong_count = 0;
-    int partner_rank = (world_rank + 1) % 4;
+    int partner_rank = (world_rank + 2) % 4; // Opposite team partner
+    int teammate_rank = (world_rank % 2 == 0) ? world_rank + 2 : world_rank - 2;
+
     while (ping_pong_count < PING_PONG_LIMIT) {
         if (world_rank == ping_pong_count % 4) {
+            // Increment and send to the opposite team
             ping_pong_count++;
             MPI_Send(&ping_pong_count, 1, MPI_INT, partner_rank, 0, MPI_COMM_WORLD);
-            printf("P%d sent and incremented ping_pong_count %d to P%d\n",
-                   world_rank, ping_pong_count, partner_rank);
+            printf("P%d sent and incremented ping_pong_count %d to P%d\n", world_rank, ping_pong_count, partner_rank);
+
+            // Inform the teammate
+            MPI_Send(&ping_pong_count, 1, MPI_INT, teammate_rank, 0, MPI_COMM_WORLD);
+            printf("P%d informed teammate P%d about ping_pong_count %d\n", world_rank, teammate_rank, ping_pong_count);
         } else {
-            MPI_Recv(&ping_pong_count, 1, MPI_INT, (world_rank == 0 ? 3 : world_rank - 1), 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("P%d received ping_pong_count %d from P%d\n",
-                   world_rank, ping_pong_count, (world_rank == 0 ? 3 : world_rank - 1));
+            MPI_Status status;
+            MPI_Recv(&ping_pong_count, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+            printf("P%d received ping_pong_count %d from P%d\n", world_rank, ping_pong_count, status.MPI_SOURCE);
+
+            // Check if the source is the teammate or the opponent
+            if (status.MPI_SOURCE == teammate_rank) {
+                // Received from teammate, no need to inform again
+                printf("P%d acknowledged ping_pong_count %d from teammate P%d\n", world_rank, ping_pong_count, teammate_rank);
+            } else {
+                // Received from opponent, inform the teammate
+                MPI_Send(&ping_pong_count, 1, MPI_INT, teammate_rank, 0, MPI_COMM_WORLD);
+                printf("P%d informed teammate P%d about ping_pong_count %d\n", world_rank, teammate_rank, ping_pong_count);
+            }
         }
     }
 
     MPI_Finalize();
     return 0;
 }
+
 
